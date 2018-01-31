16626cef004fb93bb1e8834633e7a00dec59bdec
mura0149 <31722880+mura0149@users.noreply.github.com>
2018-01-31 19:28:35 +0100

    Opgave 2/3

---
+++ Billetlib/Billetlib/Bil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billetlib
+{
+    public class Bil
+    {
+        public string Nummerplade
+        {
+            get
+            {
+                return Nummerplade;
+            }
+            
+        }
+
+        public DateTime Dato
+        {
+            get
+            {
+                return Dato;
+            }
+        }
+            public int pris()
+        {
+            return 240;
+        }
+        public string køretøj()
+        {
+            return "Bil";
+        }
+    }
+}
