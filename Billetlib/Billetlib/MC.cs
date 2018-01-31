449eecb84632786eb79e5cfcae7e594687c1bcc6
mura0149 <31722880+mura0149@users.noreply.github.com>
2018-01-31 19:37:06 +0100

    Opgave 2/3

--- Billetlib/Billetlib/MC.cs
+++ Billetlib/Billetlib/MC.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Billetlib
 {
-    class MC
+    public class MC
     {
+        public string Nummerplade
+        {
+            get
+            {
+                return Nummerplade;
+            }
+        }
+            public DateTime Dato
+        {
+            get
+            {
+                return Dato;
+            }
+        }
+        public int pris()
+        {
+            return 125;
+        }
+        public string køretøj()
+        {
+            return "Mc";
+        }
     }
 }
