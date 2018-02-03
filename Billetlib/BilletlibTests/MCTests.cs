55f727e85ae5fd726a854d5d1dcbde3975825420
mura0149 <31722880+mura0149@users.noreply.github.com>
2018-02-03 21:25:32 +0100

    MCTEST

---
+++ Billetlib/BilletlibTests/MCTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Billetlib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billetlib.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void TesprisTest()
+        {
+            MC MC1 = new MC();
+            int pris = MC1.pris();
+            Assert.AreEqual(125, pris);
+        }
+
+        [TestMethod()]
+        public void TestkøretøjTest()
+        {
+            MC Harley = new MC();
+            string køretøj = Harley.køretøj();
+            Assert.AreEqual("Mc", køretøj);
+        }
+    }
+}
