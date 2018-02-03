fb04755d6a8ba131cd76d142d8d79d341c58b138
mura0149 <31722880+mura0149@users.noreply.github.com>
2018-02-03 21:18:24 +0100

    Bil Test

---
+++ Billetlib/BilletlibTests/BilTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Billetlib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billetlib.Tests
+{
+    [TestClass()]
+    public class BilTests
+    {
+        [TestMethod()]
+        public void TestprisPris()
+        {
+            Bil bil1 = new Bil();
+            int pris = bil1.pris();
+            Assert.AreEqual(240,pris);
+        }
+
+        [TestMethod()]
+        public void TestkøretøjPris()
+        {
+            Bil bil2 = new Bil();
+            int pris = bil2.pris();
+            Assert.AreEqual(240, pris);
+        }
+    }
+}
